3ec224251a4b40bea2e1f9674883b8ffce4bff68
Evan Taylor Yates <evantayloryates@gmail.com>
2023-08-12 18:47:24 -0700

    Dummy change to test deploy action

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -11,7 +11,7 @@ export default function IndexRoute() {
     <div className="container">
       <div className="content">
         <h1>
-          Remix <span>Jokes!</span>
+          Remix <span>Jokes!</span> DEPLOY
         </h1>
       </div>
     </div>
